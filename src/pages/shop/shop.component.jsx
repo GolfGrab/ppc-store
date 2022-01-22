21d6366557a27b0e4e672f87b229e89c4b1ad65f
GolfGrab <surapus.m@ku.th>
2022-01-22 16:19:56 +0700

    add shop page with collection preview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import SHOP_DATA from './shop.data';
 
 export default function ShopPage(props) {
-  const [collection,setCollection] = useState(SHOP_DATA)
+  const [collections,setCollections] = useState(SHOP_DATA)
   return (
-    <div>Shop Page</div>
+    <div className='shop-page'>
+      {
+        collections.map(({id,...otherCollectionProps}) =>
+          <CollectionPreview key={id} {...otherCollectionProps}/>
+        )
+      }
+    </div>
   )
 }
